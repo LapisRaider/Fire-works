006b7b1ad80b0082a92718e8d89742d812a799fd
LapisRaider <36361212+LapisRaider@users.noreply.github.com>
2024-01-20 14:23:18 +0800

    Candidate script

---
+++ Assets/Scripts/Candidate.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+class Candidate
+{
+    public string Name;
+    public string Description;
+}
