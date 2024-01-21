578275cf8ec20df766066380c08e0f08caadc55c
LapisRaider <36361212+LapisRaider@users.noreply.github.com>
2024-01-21 09:03:45 +0800

    Boss scene text animation

--- Assets/Scripts/BossScene.cs
+++ Assets/Scripts/BossScene.cs
@@ -1,29 +1,52 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BossScene : SingletonBase<BossScene>
 {
-    public string TextToSay = "fhuiwerhuiferhuifer";
+    public List<string> TextToSay = new List<string>();
     private string CurrStrSaid = "";
     public TextMeshProUGUI text = null;
     public float TypeSpeed = 0.2f;
     private int CurrIndex = 0;
+    private int CurrSentence = 0;
+
+    private bool isTypeing = false;
 
     public void SayText()
     {
         StartCoroutine(SayTextIterate());
     }
 
+    public void NextSentence()
+    {
+        if (isTypeing)
+            return;
+
+        ++CurrSentence;
+        if (CurrSentence >= TextToSay.Count)
+            return;
+
+        CurrIndex = 0;
+        CurrStrSaid = "";
+        text.SetText("");
+
+        StartCoroutine(SayTextIterate());
+    }
+
     IEnumerator SayTextIterate()
     {
-        while (CurrStrSaid.Length != TextToSay.Length)
+        isTypeing = true;
+        while (CurrStrSaid.Length != TextToSay[CurrSentence].Length)
         {
-            CurrStrSaid += TextToSay[CurrIndex];
+            CurrStrSaid += TextToSay[CurrSentence][CurrIndex];
             text.SetText(CurrStrSaid);
             ++CurrIndex;
 
             yield return new WaitForSeconds(TypeSpeed);
         }
+
+        isTypeing = false;
     }
 }
